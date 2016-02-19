cde11d03d392ef89ca7b60fa5fc9ca56a8af37e6
lxx <lxx@lixiangxiangdemacbook-pro.local>
2016-02-19 10:31:03 +0800

    Initial Commit

---
+++ QQ列表/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QQ列表
+//
+//  Created by lxx on 16/2/19.
+//  Copyright © 2016年 lxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QQ列表/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QQ列表
+//
+//  Created by lxx on 16/2/19.
+//  Copyright © 2016年 lxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
